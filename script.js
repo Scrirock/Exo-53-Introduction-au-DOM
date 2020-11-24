2d7d1bd9a12d8860332b40b7a3c472a903e46f66
Scrirock <73163680+Scrirock@users.noreply.github.com>
2020-11-24 14:59:50 +0100

    hcj

---
+++ script.js
@@ -0,0 +1,49 @@
+document.getElementById("c1").style.color = "red";
+document.getElementById("c2").style.color = "red";
+document.getElementById("c3").style.color = "red";
+document.getElementById("c4").style.color = "red";
+document.getElementById("c5").style.color = "red";
+
+document.getElementById("c1").style.width = "50rem";
+document.getElementById("c2").style.width = "30rem";
+document.getElementById("c3").style.width = "80rem";
+document.getElementById("c4").style.width = "60rem";
+document.getElementById("c5").style.width = "50rem";
+
+document.getElementById("c1").style.animation = "animTest";
+document.getElementById("c1").style.animationDuration = "5s";
+document.getElementById("c4").style.animation = "animTest";
+document.getElementById("c4").style.animationDuration = "5s";
+
+document.getElementById("sp1").style.backgroundColor = "blue";
+document.getElementById("sp2").style.backgroundColor = "blue";
+document.getElementById("sp3").style.backgroundColor = "blue";
+document.getElementById("sp4").style.backgroundColor = "blue";
+document.getElementById("sp5").style.backgroundColor = "blue";
+document.getElementById("sp6").style.backgroundColor = "blue";
+document.getElementById("sp7").style.backgroundColor = "blue";
+document.getElementById("sp8").style.backgroundColor = "blue";
+document.getElementById("sp9").style.backgroundColor = "blue";
+document.getElementById("sp10").style.backgroundColor = "blue";
+document.getElementById("sp11").style.backgroundColor = "blue";
+document.getElementById("sp12").style.backgroundColor = "blue";
+document.getElementById("sp13").style.backgroundColor = "blue";
+document.getElementById("sp14").style.backgroundColor = "blue";
+document.getElementById("sp15").style.backgroundColor = "blue";
+
+document.getElementById("direction").dir = "rtl";
+
+
+document.getElementById("l1").innerText = "mon";
+document.getElementById("l2").innerText = "nouveau";
+document.getElementById("l3").innerText = "texte";
+document.getElementById("l4").innerText = "des";
+document.getElementById("l5").innerText = "liens";
+
+document.getElementById("l1").href = "https://www.google.be/";
+document.getElementById("l2").href = "https://www.google.be/";
+document.getElementById("l3").href = "https://www.google.be/";
+document.getElementById("l4").href = "https://www.google.be/";
+document.getElementById("l5").href = "https://www.google.be/";
+
+document.getElementById("c5").innerText = "le texte a changé";
